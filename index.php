449ba87df740d02bb43126ebfc646f57876b11df
helena rodrigues <helena19.rodrigues@gmail.com>
2022-08-30 21:59:59 +0200

    primerosEjerciciosPhp

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+// Ejercicio 1
+$a= 12;
+$b= 23;
+/* echo $a+$b."\n";
+echo $a-$b."\n";
+echo $a/$b."\n";
+echo $a%$b."\n"; */
+
+// ejercicio 2
+
+$x=["Coco","Fresa","Manzana","Mango","Platanos"];
+/* echo $x[3]; */
+
+// Ejercicio 3
+$c= ["Coco","Fresa",["Manzana","Mango", "Peras",["pepito", "grillo",35,"Platanos","tomates"]]];
+
+echo $c[2][2];
+
+
+?>
